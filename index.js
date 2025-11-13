d16d1b4619523886b4d1beb7a9c74ae921cc1c83
Raihan <raihanuddinahmed10@gmail.com>
2025-11-13 10:15:37 +0600

    get ready for deploy on vercel

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     const db = client.db("HomeNest");
     const propertyCollection = db.collection("properties");
     const reviewCollection = db.collection("reviews");
